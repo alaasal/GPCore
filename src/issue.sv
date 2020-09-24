7589d4900df42cde28768acd95ea438f4e38baef
Moustafa Mahmoud <e.moustafa2@gmail.com>
2020-09-24 15:54:24 +0200

    input port added to write back data to regfile

--- src/issue.sv
+++ src/issue.sv
@@ -1,19 +1,20 @@
 module issue_stage (
 	input logic clk, nrst,
 	input logic we, B_SEL,		// we enable for regfile & B_SEL for op_b or immediate
+	input logic wb_d,		// data to be written in regfile
 	input logic [31:0] inst,	// input from IF stage (inst mem)
 	output logic [31:0] op_a, op_b	// operands A & B output from regfile in PIPE #3 (to exe stage)
 	);
 
 	// registers
 	logic [31:0] instr;	// pipe #3 from inst mem to decode stage and regfile
-	
+
 	// wires
 	logic [4:0]  src_a, src_b; // addresses of required op_a & op_b from reg file
 	logic [4:0]  dest;         // address of destenation register to write back in reg file
 	logic [31:0] imm;	   // immediate sign extended
 	logic [31:0] a, b;   	   // operands value output from the register file
-	
+
 	// PIPE
 	always_ff @(posedge clk, negedge nrst)
 	  begin
@@ -31,7 +32,7 @@ module issue_stage (
 	assign imm   = 32'(signed'(instr[31:20]));  // sign extended immediate to 32-bit
 
 	// register file
-	regfile reg1 (.clk(clk), .clrn(nrst), .we(we), .write_addr(dest), .source_a(src_a), .source_b(src_b), .result(0),
+	regfile reg1 (.clk(clk), .clrn(nrst), .we(we), .write_addr(dest), .source_a(src_a), .source_b(src_b), .result(wb_d),
 			.op_a(a), .op_b(b));
 
 	// assign op_a and op_b outputs
