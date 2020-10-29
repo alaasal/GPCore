fa8d6ed776e5a4f9f4af428761f8388e6bd6824b
Moustafa Mahmoud <e.moustafa2@gmail.com>
2020-10-29 20:05:31 +0200

    csr regfile initial commit

---
+++ src/csr_regfile.sv
@@ -0,0 +1,176 @@
+`include "define.sv"
+
+module csr_regfile(
+	input logic clk, nrst,
+	input logic [11:0] csr_address_r, csr_address_wb,
+	input logic [31:0] csr_wb,		// csr data written back from csr to the register file
+	output logic [31:0] csr_data		// output to csr unit to perform operations on it
+);
+
+always_comb
+  begin
+	case(csr_address_r)
+		`CSR_MISA: csr_data = {
+        		2'b00,       // MXL = 32
+       			4'b0000,     // Reserved.
+        		/* Extensions.
+        		 *  ZYXWVUTSRQPONMLKJIHGFEDCBA */
+        		26'b00000000000001000100000001
+    			};
+		`CSR_MVENDORID:		csr_data = '0;
+		`CSR_MARCHID:		csr_data = '0;
+		`CSR_MIMPID:		csr_data = '0;
+		`CSR_MHARTID:		csr_data = '0;
+		`CSR_MSTATUS:		csr_data = mstatus;
+		`CSR_MIP:		csr_data = mip;
+		`CSR_MIE:		csr_data = mie;
+		`CSR_MTVEC:		csr_data = {mtvec, 2'b0}; // direct mode
+		`CSR_MEPC:		csr_data = {mepc, 2'b0};  // two low bits are always zero
+		`CSR_MCAUSE:		csr_data = mcause;
+		`CSR_MTVAL:		csr_data = mtval;
+		`CSR_MSCRATCH:		csr_data = mscratch;
+
+/** not implemented yet **
+		`CSR_MEDELEG:	// will be implemented in s-mode
+		`CSR_MIDELEG:	// will be implemented in s-mode
+		`CSR_MCOUNTEREN:
+		`CSR_MCYCLE:
+		`CSR_MINSTRET:
+		`CSR_MCYCLEH:
+		`CSR_MINSTRETH:
+		`CSR_CYCLEH:
+		`CSR_TIMEH:
+		`CSR_INSTRETH:
+**			    */
+
+	endcase
+  end
+
+
+// registers
+// mstatus
+logic status_sie;
+logic status_mie;
+logic status_spie;
+logic status_mpie;
+logic status_spp;
+logic status_mpp;
+logic status_sum;
+// mie
+logic meie;
+logic seie;
+logic mtie;
+logic stie;
+
+logic m_timer;
+
+logic [`XLEN-1:2] mtvec;
+
+logic [`XLEN-1:0] mscratch;
+
+// wires
+logic [`XLEN-1:0] mstatus;
+logic [`XLEN-1:0] mip;
+logic [`XLEN-1:0] mie;
+
+assign mstatus = {
+    14'b0,
+    status_sum,
+    1'b0,
+    4'b0,
+    status_mpp,
+    2'b0,
+    status_spp,
+    status_mpie,
+    1'b0,
+    status_spie,
+    1'b0,
+    status_mie,
+    1'b0,
+    status_sie,
+    1'b0
+};
+
+assign mip = {
+    20'b0,
+    m_interrupt,
+    1'b0,
+    s_interrupt,
+    1'b0,
+    m_timer,
+    1'b0,
+    s_timer,
+    5'b0
+};
+
+assign mie = {
+    20'b0,
+    meie,
+    1'b0,
+    seie,
+    1'b0,
+    mtie,
+    1'b0,
+    stie,
+    5'b0
+};
+
+always_ff @(posedge clk, negedge nrst)
+	if (!nrst)
+	  begin
+		status_sie  	<= 0;
+                status_mie  	<= 0;
+                status_spie 	<= 0;
+ 		status_mpie 	<= 0;
+                status_spp  	<= `PRIV_M;
+                status_mpp  	<= 0;
+                status_sum  	<= 0;
+		mtvec	    	<= '0;
+		mscratch	<= '0;
+		meie		<= 0;
+		seie 		<= 0;
+		mtie 		<= 0;
+		stie 		<= 0;
+		m_timer 	<= 0;
+	  end
+	else
+	  begin
+		case(csr_address_wb)
+			`CSR_MSTATUS:
+			  begin
+				status_sie  <= csr_wb[1];
+                        	status_mie  <= csr_wb[3];
+                        	status_spie <= csr_wb[5];
+                        	status_mpie <= csr_wb[7];
+                        	status_spp  <= csr_wb[8];
+                        	status_mpp  <= csr_wb[12:11];
+                        	status_sum  <= csr_wb[18];
+			  end
+			`CSR_MTVEC:
+				mtvec <= csr_wb[`XLEN:2];
+			`CSR_MEDELEG:
+				begin end
+			`CSR_MIDELEG:
+				begin end
+			`CSR_MIE:
+			  begin
+				stie <= csr_wb[5];
+                        	mtie <= csr_wb[7];
+                        	seie <= csr_wb[9];
+                        	meie <= csr_wb[11];
+			  end
+			`CSR_MSCRATCH: 
+				mscratch <= csr_wb;
+			`CSR_MEPC:
+				mepc <= csr_wb[31:2];
+			`CSR_MCAUSE:
+			  begin
+				mcause_code <= csr_wb[5:0];
+				mcause_interrupt <= csr_wb[31];
+			  end
+			`CSR_MTVAL:
+				mtval <= csr_wb;
+		endcase
+	  end
+
+endmodule
---
+++ src/define.sv
@@ -0,0 +1,62 @@
+// CSR Addr  
+package csr;
+
+// 
+
+`define XLEN 32
+
+/**************************************************************************************************/
+/**** exception & interrupt                                                                    ****/
+/**************************************************************************************************/
+`define CAUSE_INTERRUPT 32'h80000000 // msb of mcause register = 1 then interrupt not exception
+
+// if not msb = 1 then exception
+// EXEPTION CODE				// exception code according to the manual this will define the trap handler		 
+`define CAUSE_MISALIGNED_FETCH      32'h0
+`define CAUSE_FAULT_FETCH           32'h1
+`define CAUSE_ILLEGAL_INSTRUCTION   32'h2
+`define CAUSE_BREAKPOINT            32'h3
+`define CAUSE_MISALIGNED_LOAD       32'h4
+`define CAUSE_FAULT_LOAD            32'h5
+`define CAUSE_MISALIGNED_STORE      32'h6
+`define CAUSE_FAULT_STORE           32'h7
+`define CAUSE_USER_ECALL            32'h8
+`define CAUSE_SUPERVISOR_ECALL      32'h9
+`define CAUSE_HYPERVISOR_ECALL      32'ha
+`define CAUSE_MACHINE_ECALL         32'hb
+`define CAUSE_FETCH_PAGE_FAULT      32'hc
+`define CAUSE_LOAD_PAGE_FAULT       32'hd
+`define CAUSE_STORE_PAGE_FAULT      32'hf
+
+// PRIV MODE
+`define PRIV_U 0
+`define PRIV_S 1
+`define PRIV_H 2
+`define PRIV_M 3
+
+// Machine-Mode
+`define CSR_MVENDORID   12'hf11			//Machine Vendor ID Register (mvendorid) [p18] 
+`define CSR_MARCHID     12'hf12			//Machine Architecture ID Register (marchid)  [p18]
+`define CSR_MIMPID      12'hf13			//Machine Implementation ID Register (mimpid) p[19]
+`define CSR_MHARTID     12'hf14			//Hart ID Register (mhartid) p[19]
+`define CSR_MSTATUS     12'h300			//Machine Status Registers (mstatus and mstatush) p[20]
+`define CSR_MISA        12'h301			//Machine ISA Register (misa) [p15]
+`define CSR_MEDELEG     12'h302			//Machine Trap Delegation Registers (medeleg and mideleg p[29]
+`define CSR_MIDELEG     12'h303			//Machine Trap Delegation Registers (medeleg and mideleg p[29]
+`define CSR_MIE         12'h304			//Machine Interrupt Registers (mip and mie) p[31]
+`define CSR_MTVEC       12'h305			//Machine Trap-Vector Base-Address Register (mtvec) p[28]
+`define CSR_MCOUNTEREN  12'h306
+`define CSR_MSCRATCH    12'h340
+`define CSR_MEPC        12'h341
+`define CSR_MCAUSE      12'h342
+`define CSR_MTVAL       12'h343
+`define CSR_MIP         12'h344			//Machine Interrupt Registers (mip and mie) p[31]
+`define CSR_MCYCLE      12'hb00			//**Hardware Performance Monitor p[35]
+`define CSR_MINSTRET    12'hb02			//**Hardware Performance Monitor p[35]
+`define CSR_MCYCLEH     12'hb80
+`define CSR_MINSTRETH   12'hb82
+`define CSR_CYCLEH      12'hc80
+`define CSR_TIMEH       12'hc81			//**Machine Timer Registers (mtime and mtimecmp) p[33]
+`define CSR_INSTRETH    12'hc82
+
+endpackage
