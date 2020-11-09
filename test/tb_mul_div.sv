3fc57132f45dea09c31478469e5a72c8a5f6d990
Omar Younis <56393026+OYounis@users.noreply.github.com>
2020-11-09 23:11:01 +0200

    Update mul_div tests

--- test/tb_mul_div.sv
+++ test/tb_mul_div.sv
@@ -3,7 +3,23 @@
 class packet; // used for randomization of variables 
 rand logic signed [31:0] a_rand;
 rand logic signed [31:0] b_rand;
-rand logic [2:0] mulDiv_op_rand;
+rand logic [2:0] mulDiv_op_rand; 
+
+constraint u_con{ //unsigned constraint
+  a_rand > 0;
+  b_rand > 0;
+  mulDiv_op_rand inside {3'b011, 3'b100, 3'b110}; //mulhu, divu, remu
+}
+
+constraint s_con{ //signed constraint
+  mulDiv_op_rand inside {3'b000, 3'b001, 3'b101, 3'b111}; //mul, mulh, div, rem
+}
+
+constraint su_con{ //signed unsigned constraint
+  a_rand > 0;
+  mulDiv_op_rand inside {3'b010}; //mulhsu
+}
+
 endclass
 
 module tb_mul_div ; 
@@ -25,13 +41,21 @@ module tb_mul_div ;
       logic [31:0] real_res;
       packet pkt;
       pkt = new(); 
-      $display("-----------------TESTING (Signed A, Signed B)-----------------");
+      $display("-------------------------------------------------TESTING------------------------------------------------");
+      $display("| %6s | %13s | %13s | %13s | %13s | %20s | %4s |", "OpCode", "A", "B", "DUT", "Real Result", "Full Result", "Pass");
+      $display("--------------------------------------------------------------------------------------------------------");
 
-      repeat (30)begin
+      for (int i = 0; i < 3000; i++)begin
+        
+        pkt.constraint_mode(0); //disable all constraints
+        if (i < 1000) pkt.u_con.constraint_mode(1);
+        else if (i < 2000) pkt.s_con.constraint_mode(1);
+        else pkt.su_con.constraint_mode(1);
+
         pkt.randomize();
         a         = pkt.a_rand;
         b         = pkt.b_rand;
-        mulDiv_op = 2'b00;
+        mulDiv_op = pkt.mulDiv_op_rand;
 
         // 000 -> mul 	
 	      // 001 -> mulh
@@ -40,8 +64,9 @@ module tb_mul_div ;
 	      // 101 -> div 
 	      // 100 -> divu
 	      // 111 -> rem
-	      // 111 -> remu
+	      // 110 -> remu
 
+        //the following three assignements are really, really stupid. will change in future but not now, feeling lazy.
         op_type = (mulDiv_op == 3'b000)? "MUL":
                   (mulDiv_op == 3'b001)? "MULH":
                   (mulDiv_op == 3'b010)? "MULHSU":
@@ -50,14 +75,30 @@ module tb_mul_div ;
                   (mulDiv_op == 3'b100)? "DIVU":
                   (mulDiv_op == 3'b111)? "REM":
                   (mulDiv_op == 3'b110)? "REMU": "INVAL";
-
-        real_res_reg = $signed(a) * $signed(b);
-        real_res = real_res_reg[31:0];  
+        
+        
+        real_res_reg = (mulDiv_op == 3'b000)? a * b:
+                       (mulDiv_op == 3'b001)? a * b:
+                       (mulDiv_op == 3'b010)? a * b:
+                       (mulDiv_op == 3'b011)? a * b:
+                       (mulDiv_op == 3'b101)? a / b:
+                       (mulDiv_op == 3'b100)? a / b:
+                       (mulDiv_op == 3'b111)? a % b:
+                       (mulDiv_op == 3'b110)? a % b: 64'hx;
 
         
-  
+        real_res = (mulDiv_op == 3'b000)? real_res_reg[31:0]:
+                   (mulDiv_op == 3'b001)? real_res_reg[63:32]:
+                   (mulDiv_op == 3'b010)? real_res_reg[63:32]:
+                   (mulDiv_op == 3'b011)? real_res_reg[63:32]:
+                   (mulDiv_op == 3'b101)? real_res_reg[31:0]:
+                   (mulDiv_op == 3'b100)? real_res_reg[31:0]:
+                   (mulDiv_op == 3'b111)? real_res_reg[31:0]:
+                   (mulDiv_op == 3'b110)? real_res_reg[31:0]: 64'hx;
         #1;
-        $display("\top_type = %s \tA = %d \tB = %d \ttb_res = %d \treal_res = %d \tfull_res = %d", op_type, a, b, res, real_res, real_res_reg);
+        //$display("\top_type = %5s\t \tA = %d \tB = %d \tDUT_res = %d \treal_res = %d \tfull_res = %d \tpass = %d ", op_type, a, b, res, real_res, real_res_reg, real_res == res);
+        $display("| %6s | %13d | %13d | %13d | %13d | %20d | %4d |", op_type, a, b, res, real_res, real_res_reg, real_res == res);
+        $display("--------------------------------------------------------------------------------------------------------");
       end
     end   
 endmodule
