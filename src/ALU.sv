da37b610dd6c39080d82588ce4948526dd1083b5
Omar Younis <56393026+OYounis@users.noreply.github.com>
2020-12-14 18:24:52 +0200

    ALU: change nonblocking assignments to blocking

--- src/ALU.sv
+++ src/ALU.sv
@@ -26,20 +26,20 @@ module alu #(
 );
 
     //TODO: move signal decoding to a seperate always block
-    logic sub_op;                           //subtract operation
-    logic sll_op, srl_op, sra_op;           //shift operations
-    logic slt_op, sltu_op, bge_op, bgeu_op; //comparison operations
+    logic sub_op;           //subtract operation
+    logic sll_op, sra_op;   //shift operations
+    logic slt_op, bge_op;   //comparison operations
     //operation decode
     always_comb begin
-        sub_op <= alu_fn[3];                //false triggers at SRA, BGE, BGEU. gate count reduction
+        sub_op = alu_fn[3];                //false triggers at SRA, BGE, BGEU. gate count reduction
 
-        sll_op <= ~alu_fn[3] & ~alu_fn[2];  //false triggers at ADD, SLT, SLTU
+        sll_op = ~alu_fn[3] & ~alu_fn[2];  //false triggers at ADD, SLT, SLTU
         //srl_op <= (alu_fn == SRL);  //not really required 
-        sra_op <= alu_fn[3] & alu_fn[2];    //no false triggers
+        sra_op = alu_fn[3] & alu_fn[2];    //no false triggers
 
-        slt_op  <= ~alu_fn[3] & alu_fn[1] &  ~alu_fn[0]; 
+        slt_op  = ~alu_fn[3] & alu_fn[1] &  ~alu_fn[0]; 
         //sltu_op <= (alu_fn == SLTU);    //not really required
-        bge_op  <= alu_fn[3] & alu_fn[0];
+        bge_op  = alu_fn[3] & alu_fn[0];
         //bgeu_op <= (alu_fn == BGEU);    //not really required
     end
     /***************************************************************************************************************/
@@ -47,17 +47,16 @@ module alu #(
     logic signed [31:0] b_adder;    //operand b input to adder
     logic signed [31:0] a_adder;    //operand a input to adder
     logic signed [31:0] adder_result;      //33 adder output, bit 32 is carry out(overflow indication)
-    logic [31:0] b_negate;
-    logic cin, cout;                      //adder carry-in, mapped to fpga Cin
+    logic b_negate;
+    logic cout;                      //adder carry-in, mapped to fpga Cin
     //adder
     always_comb begin
-        b_negate <= sub_op;
+        b_negate = sub_op;
 
-        b_adder  <= {32{b_negate}} ^ operandB;
-        cin      <= sub_op;
-        a_adder  <= operandA;
+        b_adder  = {32{b_negate}} ^ operandB;
+        a_adder  = operandA;
 
-        {cout ,adder_result} <= a_adder + b_adder + cin;    //output of add, sub
+        {cout, adder_result} = a_adder + b_adder + b_negate;    //output of add, sub
     end
     /***************************************************************************************************************/
     /***************************************************************************************************************/
@@ -70,16 +69,16 @@ module alu #(
     logic signed [31:0] shifter_result;     //final shifter result
     //shifter
     always_comb begin
-        a_reversed     <= {<<{operandA}};    //reverse operand a
-        shift_op_a     <= (sll_op)? a_reversed : operandA;
-        shift_amt      <= operandB;
+        a_reversed     = {<<{operandA}};    //reverse operand a
+        shift_op_a     = (sll_op)? a_reversed : operandA;
+        shift_amt      = operandB;
         
-        shift_l        <= shift_op_a >> shift_amt;
-        shift_a        <= shift_op_a >>> shift_amt;
-        shift_reversed <= {<<{shift_l}};
+        shift_l        = shift_op_a >> shift_amt;
+        shift_a        = shift_op_a >>> shift_amt;
+        shift_reversed = {<<{shift_l}};
 
-        shift_l_res    <= (sll_op)? shift_reversed : shift_l;
-        shifter_result <= (sra_op)? shift_a : shift_l_res;   //final output
+        shift_l_res    = (sll_op)? shift_reversed : shift_l;
+        shifter_result = (sra_op)? shift_a : shift_l_res;   //final output
     end
     /***************************************************************************************************************/
     /***************************************************************************************************************/
@@ -90,15 +89,15 @@ module alu #(
     logic [31:0] comparator_result; //comparator output
     //comparator
     always_comb begin
-        op_signed <= slt_op | bge_op;
+        op_signed = slt_op | bge_op;
 
-        a_comp <= operandA;
-        b_comp <= operandB;
+        a_comp = operandA;
+        b_comp = operandB;
 
-        less_unsigned <= a_comp < b_comp;
-        less_signed   <= $signed(a_comp) < $signed(b_comp);
+        less_unsigned = a_comp < b_comp;
+        less_signed   = $signed(a_comp) < $signed(b_comp);
 
-        comparator_result <= (op_signed)? less_signed : less_unsigned;
+        comparator_result = (op_signed)? less_signed : less_unsigned;
     end
     /***************************************************************************************************************/
     /***************************************************************************************************************/
@@ -106,29 +105,29 @@ module alu #(
     always_comb begin
         case (alu_fn)
             //logical operations
-            AND: result <= operandA & operandB; 
-            OR:  result <= operandA | operandB;
-            XOR: result <= operandA ^ operandB;
+            AND: result = operandA & operandB; 
+            OR:  result = operandA | operandB;
+            XOR: result = operandA ^ operandB;
             
             //arithmetic operations
-            ADD, SUB: result <= adder_result;
+            ADD, SUB: result = adder_result;
 
             //shift operations
-            SLL, SRL, SRA: result <= shifter_result;
+            SLL, SRL, SRA: result = shifter_result;
             
             //comparator operations
-            SLT, SLTU, BGE, BGEU: result <= comparator_result;
-            default: result <= 32'b0;
+            SLT, SLTU, BGE, BGEU: result = comparator_result;
+            default: result = 32'b0;
         endcase    
     end
 
     //branch block
     always_comb begin
         case (alu_fn)
-            SUB: btaken <= ((|result) & bneq) || ((~|result) & ~bneq);
-            SLT, SLTU: btaken <= result[0];
-            BGE, BGEU: btaken <= ~result[0];
-            default: btaken <= 0;
+            SUB: btaken = ((|result) & bneq) || ((~|result) & ~bneq);
+            SLT, SLTU: btaken = result[0];
+            BGE, BGEU: btaken = ~result[0];
+            default: btaken = 0;
         endcase
     end
 endmodule
