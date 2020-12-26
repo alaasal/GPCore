c96a5592117699ef4657d7892d796a1b291789bf
Alaa Salman <38817722+alaasal@users.noreply.github.com>
2020-12-26 03:39:53 +0200

    minor change

--- src/core.sv
+++ src/core.sv
@@ -129,7 +129,7 @@ if (!nrst)
 mem_to_piton <= 0;
 else
 begin
-	if ( (!mem_to_piton) && (~|state_reg) && (!l15_transducer_header_ack) && mem_op3 )
+	if ( (!mem_to_piton) && (~|state_reg) && (!l15_transducer_header_ack) && |mem_op3 )
 	begin
 		mem_to_piton <= 1;
 	end
