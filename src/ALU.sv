6bf0e7805b7dd337a29745a99f40a1a5a93544a2
alexcore <mohamednour98@github.com>
2021-02-03 01:48:28 +0200

    minorfixes

--- src/ALU.sv
+++ src/ALU.sv
@@ -14,11 +14,11 @@ module alu(
 	  begin
 		unique case(alu_fn)
 			4'b0000: result = $signed(operandA) + $signed(operandB);
-			4'b0001: result = $signed(operandA) << $signed(operandB);
+			4'b0001: result = $signed(operandA) << $signed(operandB[4:0]);
 			4'b0010: result = ($signed(operandA) < $signed(operandB));
 			4'b0011: result = (operandA < operandB); 
 			4'b0100: result = $signed(operandA) ^ $signed(operandB);
-			4'b0101: result = $signed(operandA) >> $signed(operandB);
+			4'b0101: result = $signed(operandA) >> $signed(operandB[4:0]);
 			4'b0110: result = $signed(operandA) | $signed(operandB);
 			4'b0111: result = $signed(operandA) & $signed(operandB);
 			4'b1000: result = $signed(operandA) - $signed(operandB);
@@ -26,7 +26,7 @@ module alu(
 			4'b1010: result = (operandA > operandB);  		    // for bgeu
 			//5'b01011: result = 0;
 			//5'b01100: result = 0;
-			4'b1101: result = $signed(operandA) >>> $signed(operandB);
+			4'b1101: result = $signed(operandA) >>> $signed(operandB[4:0]);
 			
 			default: result = 0;
 		endcase
